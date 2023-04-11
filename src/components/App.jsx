0c01a3d91102e6abc651dc62c0f0b05956001ebb
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-11 19:34:54 +0300

    refactor with useState hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,50 +1,98 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { GlobalStyle } from './GlobalStyle';
 import { Layout } from './Layout/Layout';
 import { Section } from './Section/Section';
 import { Statistics } from './Statistics/Statistics';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  leaveFeedback = e => {
+  const leaveFeedback = e => {
     //console.log(e); // good
-    this.setState({ [e]: this.state[e] + 1 });
+    switch (e) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      default:
+        return;
+    }
   };
 
-  countTotalFeedback = ({ good, neutral, bad }) => good + neutral + bad;
+  const countTotalFeedback = () => good + neutral + bad;
 
-  countPositiveFeedbackPercentage = ({ good, neutral, bad }) =>
-    Math.round((good * 100) / this.countTotalFeedback(this.state));
+  const countPositiveFeedbackPercentage = () =>
+    Math.round((good * 100) / countTotalFeedback());
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    return (
-      <Layout>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.leaveFeedback}
-          />
-        </Section>
-        <Section title="Statistics">
-          <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={this.countTotalFeedback(this.state)}
-            positivePercentage={this.countPositiveFeedbackPercentage(
-              this.state
-            )}
-          ></Statistics>
-        </Section>
-        <GlobalStyle />
-      </Layout>
-    );
-  }
+  return (
+    <Layout>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={Object.keys({ good, neutral, bad })}
+          onLeaveFeedback={leaveFeedback}
+        />
+      </Section>
+      <Section title="Statistics">
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={countTotalFeedback()}
+          positivePercentage={countPositiveFeedbackPercentage()}
+        ></Statistics>
+      </Section>
+      <GlobalStyle />
+    </Layout>
+  );
 }
+
+// export class App extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+
+//   leaveFeedback = e => {
+//     this.setState({ [e]: this.state[e] + 1 });
+//   };
+
+//   countTotalFeedback = ({ good, neutral, bad }) => good + neutral + bad;
+
+//   countPositiveFeedbackPercentage = ({ good, neutral, bad }) =>
+//     Math.round((good * 100) / this.countTotalFeedback(this.state));
+
+//   render() {
+//     const { good, neutral, bad } = this.state;
+//     return (
+//       <Layout>
+//         <Section title="Please leave feedback">
+//           <FeedbackOptions
+//             options={Object.keys(this.state)}
+//             onLeaveFeedback={this.leaveFeedback}
+//           />
+//         </Section>
+//         <Section title="Statistics">
+//           <Statistics
+//             good={good}
+//             neutral={neutral}
+//             bad={bad}
+//             total={this.countTotalFeedback(this.state)}
+//             positivePercentage={this.countPositiveFeedbackPercentage(
+//               this.state
+//             )}
+//           ></Statistics>
+//         </Section>
+//         <GlobalStyle />
+//       </Layout>
+//     );
+//   }
+// }
